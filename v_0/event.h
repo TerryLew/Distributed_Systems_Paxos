efad8f2b783623edb1946be0437286e9124c5272
TerryLew <terrysatou@gmail.com>
2017-11-15 21:00:41 -0500

    prototype

--- v_0/event.h
+++ v_0/event.h
@@ -1,3 +1,4 @@
+
 class Event{
 private:
     int _userId;
@@ -20,27 +21,3 @@ public:
     
     void populate(int userId, string op, string data, struct tm* localTime, struct tm* utcTime);
 };
-=======
-class Event{
-private:
-    int _userId;
-    string _op;
-    string _data;
-    struct tm* _localTime;
-    struct tm* _utcTime;
-    
-public:
-    
-    int getUserId() { return _userId; }
-    string getOp() { return _op; }
-    string getData() { return _data; }
-    struct tm* getLocalTime { return _localTime; }
-    struct tm* getUtcTime() { return _utcTime; }
-    
-    const string serialize() const;
-    const string serializeForView() const;
-    const string serializeForStoring() const;
-    
-    void populate(int userId, string op, string data, struct tm* localTime, struct tm* utcTime);
-};
->>>>>>> 7f02ef0a5c923f4e0327372befce5a6fe6e1174e
--- v_0/paxosmsg.h
+++ v_0/paxosmsg.h
@@ -1,5 +1,7 @@
+
 class PaxosMsg {
 private:
+    string _type;
     int _proposer;
     int _slot;
     int _num;
@@ -11,6 +13,7 @@ public:
     PaxosMsg(string type, int accNum, Event value);
     PaxosMsg(string type, int slot, int accNum, Event value);
     
+    string getType() { return _type; }
     int getProposer() { return _proposer; }
     int getSlot() { return _slot; }
     int getNum() { return _num; }
@@ -19,26 +22,3 @@ public:
     string serialize();
     void deserialize(string& msg);
 };
-=======
-class PaxosMsg {
-private:
-    int _proposer;
-    int _slot;
-    int _num;
-    Event _value;
-    
-public:
-    PaxosMsg(string& msg);
-    PaxosMsg(string type, int num);
-    PaxosMsg(string type, int accNum, Event value);
-    PaxosMsg(string type, int slot, int accNum, Event value);
-    
-    int getProposer() { return _proposer; }
-    int getSlot() { return _slot; }
-    int getNum() { return _num; }
-    Event getValue() { return _value; }
-    
-    string serialize();
-    void deserialize(string& msg);
-};
->>>>>>> 7f02ef0a5c923f4e0327372befce5a6fe6e1174e
